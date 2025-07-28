03b917f4227d68e906c8521a3bc368e1d8df09e2
ElijahLewes <elsauerwalt@gmail.com>
2025-07-28 08:46:39 -0400

    Fixing OnPageLoads behavior because I cannot read and forgot that if you don't return something it does not exist

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import OnPageLoad from './components/OnPageLoad.jsx'
 import  Navbar from './components/Navbar.jsx'
 import Home from './pages/Home.jsx'
 import Link212 from './pages/Link212.jsx'
 import Team from './pages/Team.jsx'
 import ContactUs from './pages/ContactUs.jsx'
 import Remington from './pages/NeighborhoodPages/Remington.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 //import Link212 from './pages/Link212.jsx' // Uncomment if you want to use
 
 //doubles as landign page
@@ -18,7 +19,7 @@ function App() {
 
   return (
     <>
-      <Navbar />  
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/link212" element={<Link212 />} />
@@ -27,6 +28,8 @@ function App() {
         <Route path="/neighborhoods/remington" element={<Remington />} />
         {/* Add more routes as needed */}
       </Routes>
+     <OnPageLoad /> 
+
     </>
   )
 }
--- src/components/OnPageLoad.jsx
+++ src/components/OnPageLoad.jsx
@@ -5,10 +5,12 @@ import { useLocation } from 'react-router-dom';
 // Defines default behavior for scrolling to the top of the page on route change
 // This component should be used in the main App component to ensure it runs on every route change
 
-function ScrollToTop() {
+function OnPageLoad() {
   const { pathname } = useLocation();
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
   return null;
 }
+
+export default OnPageLoad;
